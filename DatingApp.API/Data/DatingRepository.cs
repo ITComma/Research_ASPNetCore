c9cb62264c239193a12df25495963a4a4fb6807b
Lưu Đức Trung <luuductrung1234@gmail.com>
2019-01-25 09:55:23 +0700

    > UPDATE : implement Message System in backend ASP.NET Core

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -114,9 +114,12 @@ namespace DatingApp.API.Data
     {
       var user = await _context.Users.Include(x => x.Likers).Include(x => x.Likees).FirstOrDefaultAsync(u => u.Id == id);
 
-      if(likers) {
+      if (likers)
+      {
         return user.Likers.Where(u => u.LikeeId == id).Select(i => i.LikerId);
-      }else{
+      }
+      else
+      {
         return user.Likees.Where(u => u.LikerId == id).Select(i => i.LikeeId);
       }
     }
@@ -125,6 +128,47 @@ namespace DatingApp.API.Data
     public async Task<bool> SaveAll()
     {
       return await _context.SaveChangesAsync() > 0;
+    }
+
+    public async Task<Message> GetMessage(int id)
+    {
+      return await _context.Messages.FirstOrDefaultAsync(m => m.Id == id);
+    }
+
+    public async Task<PagedList<Message>> GetMessagesForUser(MessageParams messageParams)
+    {
+      var messages = _context.Messages
+        .Include(u => u.Sender).ThenInclude(p => p.Photos)
+        .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+        .AsQueryable();
+
+      switch (messageParams.MessageContainer)
+      {
+        case "Inbox":
+          messages = messages.Where(u => u.Recipient.Id == messageParams.UserId);
+          break;
+        case "Outbox":
+          messages = messages.Where(u => u.Sender.Id == messageParams.UserId);
+          break;
+        default:
+          messages = messages.Where(u => u.Recipient.Id == messageParams.UserId && u.IsRead == false);
+          break;
+      }
+
+      messages = messages.OrderByDescending(m => m.MessageSent);
+      return await PagedList<Message>.CreateAsync(messages, messageParams.PageIndex, messageParams.PageSize);
+    }
+
+    public async Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
+    {
+      var messages = await _context.Messages
+        .Include(u => u.Sender).ThenInclude(p => p.Photos)
+        .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+        .Where(m => m.RecipientId == userId && m.SenderId == recipientId || m.RecipientId == recipientId && m.SenderId == userId)
+        .OrderByDescending(m => m.MessageSent)
+        .ToListAsync();
+
+      return messages;
     }
   }
 }
